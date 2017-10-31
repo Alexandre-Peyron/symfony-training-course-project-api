5258f24841ff53067dce972b39f5fd5e832206d4
Alexandre Peyron <alpeyron@gmail.com>
2017-10-31 10:36:28 +0100

    add login form and route, token generation

--- src/ApiBundle/Controller/ApiUserController.php
+++ src/ApiBundle/Controller/ApiUserController.php
@@ -3,7 +3,9 @@
 namespace ApiBundle\Controller;
 
 use ApiBundle\Entity\User;
+use ApiBundle\Entity\UserAuthToken;
 use ApiBundle\Form\UserType;
+use ApiBundle\Form\LoginType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,6 +19,48 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ApiUserController extends ApiBaseController
 {
+    /**
+     * @route("/login")
+     * @method("POST")
+     */
+    public function loginAction(Request $request)
+    {
+        $loginUser = new User();
+        $form = $this->createForm(LoginType::class, $loginUser);
+
+        $form->submit($request->request->all());
+
+        if (!$form->isValid()) {
+            return $this->createJsonResponse($form->getErrors());
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('ApiBundle:User')->findOneByUsername($loginUser->getUsername());
+
+        if (!$user) { // L'utilisateur n'existe pas
+            return $this->createJsonResponse(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        $encoder = $this->get('security.password_encoder');
+        $isPasswordValid = $encoder->isPasswordValid($user, $loginUser->getPLainPassword());
+
+        if (!$isPasswordValid) { // Le mot de passe n'est pas correct
+            return $this->createJsonResponse(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        $authToken = new UserAuthToken();
+        $authToken->setValue(base64_encode(random_bytes(50)));
+        $authToken->setCreatedAt(new \DateTime('now'));
+        $authToken->setUser($user);
+
+        $em->persist($authToken);
+        $em->flush();
+
+        return $this->createJsonResponse($authToken, Response::HTTP_CREATED, ['auth-token']);
+    }
+
+
     /**
      * @Route("/register")
      * @Method("POST")
--- src/ApiBundle/Form/LoginType.php
+++ src/ApiBundle/Form/LoginType.php
@@ -3,44 +3,35 @@
 namespace ApiBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+/**
+ * Class LoginType
+ *
+ * @package AppBundle\Form
+ */
+class LoginType extends AbstractType
 {
     /**
-     * {@inheritdoc}
+     * @param FormBuilderInterface $builder
+     * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('username')
-            ->add('email', EmailType::class)
-            ->add('firstname')
-            ->add('lastname')
-            ->add('plainPassword');
+        $builder->add('username');
+        $builder->add('plainPassword');
     }
 
     /**
-     * {@inheritdoc}
+     * @param OptionsResolver $resolver
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class'        => 'ApiBundle\Entity\User',
-            'validation_groups' => ['registration'],
+            'validation_groups' => ['login'],
             'csrf_protection'   => false
         ]);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBlockPrefix()
-    {
-        return 'apibundle_user';
-    }
-
-
 }
